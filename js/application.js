e1e55f5a35b112d63039785815abb4663a937a23
Taha Jahangir <mtjahangir@gmail.com>
2014-03-29 17:26:15 +0430

    Add an undo function

--- js/application.js
+++ js/application.js
@@ -1,4 +1,17 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  window.game = game;
+  window.states = [];
+  game.inputManager.on("move", function(){
+    window.states.push(game.storageManager.getGameState());
+  });
+  window.undo = function() {
+    var state = window.states.pop();
+    if (state) {
+    	game.storageManager.setGameState(state);
+    	game.setup();
+    	game.actuator.continueGame(); // clear loose message
+    }
+  };
 });
